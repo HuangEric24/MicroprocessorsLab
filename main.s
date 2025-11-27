0edb03f334ee2ddefa38e31288020bee56128aab
IC\eh1823 <eh1823@ic.ac.uk>
2025-11-27 12:54:18 +0000

    no message

--- main.s
+++ main.s
@@ -2,7 +2,7 @@
 
 psect	code, abs
 	
-data_out	EQU 0x06 
+data_out	EQU 0x95 
 temp	        EQU 0x90 
 delay_number	EQU 0x91  
 loop_number     EQU 0x92
@@ -17,11 +17,13 @@ main:
 start:
 	movlw 	0x0
 	movwf	TRISJ, A
-	movlw   0x00
-	movwf   counter
-	movlw   0x7F
+	movlw   0x01
+	movwf   counter_high
+	movlw   0x01
+	movwf   counter_low
+	movlw   65
 	movwf   loop_number ; 7F = 128 loop
-	movlw   0x10
+	movlw   16
 	movwf   temp; Port C all outputs
 	movwf   delay_number   ;original
 	bra 	loop_high
@@ -33,23 +35,24 @@ loop_high:
 	movff   delay_number, temp
 	incf    counter_high, F
 	movf    counter_high, W      ; W = counter
-	cpfsgt  loop_number         ; skip if counter > loop number
-	bra     test_high
+	cpfslt  loop_number         ; skip if counter > loop number
+	bra     loop_high
 	bra     loop_low
 loop_low:
 	movlw 	0x00
+	movwf   data_out
 	movff 	data_out, PORTJ
 	call    delay
 	movff   delay_number, temp
 	incf    counter_low, F
 	movf    counter_low, W      ; W = counter
-	cpfsgt  loop_number         ; skip if counter > loop number
-	bra     test_low
+	cpfslt  loop_number         ; skip if counter > loop number
+	bra     loop_low
 	goto    0x0
 	
 
-delay:
-        decfsz  delay_number
+delay:        
+	decfsz  temp
 	bra     delay
 	return
         end	main
