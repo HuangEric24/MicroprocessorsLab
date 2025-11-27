bfb502b74dca05e5007e0491f654a731d4c6ed8d
IC\eh1823 <eh1823@ic.ac.uk>
2025-11-27 11:35:52 +0000

    no message

--- main.s
+++ main.s
@@ -1,50 +1,58 @@
-#include <xc.inc>
-psect code, abs
+	#include <xc.inc>
+
+psect	code, abs
+	
+data_out	EQU 0x06 
+temp	        EQU 0x90 
+delay_number	EQU 0x91  
+loop_number     EQU 0x92
+counter_low     EQU 0x93 	
+counter_high    EQU 0x94 	
+
 main:
-    org 0x0
-    goto start
+	org	0x0
+	goto	start
 
-    org 0x100           ; Main code starts here at address 0x100
+	org	0x100		    ; Main code starts here at address 0x100
 start:
-    movlw   0x0
-    movwf   TRISJ, A    ; Port J all outputs
+	movlw 	0x0
+	movwf	TRISJ, A
+	movlw   0x00
+	movwf   counter
+	movlw   0x7F
+	movwf   loop_number ; 7F = 128 loop
+	movlw   0x10
+	movwf   temp; Port C all outputs
+	movwf   delay_number   ;original
+	bra 	loop_high
+loop_high:
+	movlw 	0xFF
+	movwf   data_out
+	movff 	data_out, PORTJ
+	call    delay
+	movff   delay_number, temp
+	incf    counter_high, F
+	movf    counter_high, W      ; W = counter
+	cpfsgt  loop_number         ; skip if counter > loop number
+	bra     test_high
+	bra     loop_low
+loop_low:
+	movlw 	0x00
+	movff 	data_out, PORTJ
+	call    delay
+	movff   delay_number, temp
+	incf    counter_low, F
+	movf    counter_low, W      ; W = counter
+	cpfsgt  loop_number         ; skip if counter > loop number
+	bra     test_low
+	goto    0x0
+	
 
-    ; Initialize registers
-    ; 0x06 = current output value (0 or 255 for square wave)
-    ; 0x90 = delay counter for frequency
-
-    movlw   0xFF        ; Start with HIGH state (255)
-    movwf   0x06, A
-
-    movlw   119         ; Delay value for "Do" frequency (261.63 Hz)
-    movwf   0x91, A     ; Store as reference value
-
-loop:
-    ; Output current square wave state to DAC
-    movff   0x06, PORTJ
-
-    ; Delay to control frequency
-    movff   0x91, 0x90  ; Reload delay counter
-    call    delay
-
-    ; Toggle between 0 and 255 (square wave)
-    movlw   0xFF
-    cpfseq  0x06, A     ; If currently 255...
-    bra     set_high    ; ...go to set_high
-
-set_low:
-    movlw   0x00        ; Set to LOW (0)
-    movwf   0x06, A
-    bra     loop
-
-set_high:
-    movlw   0xFF        ; Set to HIGH (255)
-    movwf   0x06, A
-    bra     loop
 delay:
-    decfsz  0x90, F, A
-    bra     delay
-    return
+        decfsz  delay_number
+	bra     delay
+	return
+        end	main
     
     
     
